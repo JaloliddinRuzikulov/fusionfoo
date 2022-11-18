882de194c08397135ae5235c3259e166cc3ec4ba
jaloliddin_ruzikulov <jaloliddin1ruzikulov@gmail.com>
2022-11-19 01:31:42 +0500

    New

--- tg/urls.py
+++ tg/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index),  
-    # path('<int:uid>/',views.tgmap),      
-    # path('<int:uid>/<str:lng>/',views.tgmap),      
-    path('<int:uid>/lng<str:lng>lat<str:lat>/',views.tgmap),  
-    path('products/<int:uid>/<str:sid>/<str:lat>/',views.tgmap),      
+    # path('', views.index),  
+    path('',views.tgmap),  
+    path('products/',views.products), #<int:uid>/<str:sid>/     
 ]
