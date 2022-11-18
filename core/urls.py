2938adaf5252862dd9c0e74cc5483026299f9d79
jaloliddin_ruzikulov <jaloliddin1ruzikulov@gmail.com>
2022-11-19 02:34:50 +0500

    new

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('tg/', include('tg.urls')),    
+    path('', include('tg.urls')),    
     path('admin/', admin.site.urls),
 ]
